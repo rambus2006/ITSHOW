60bd36d4bd752dce14b246dd2091decf2902fef2
rambus2006 <w2227@e-mirim.hs.kr>
2024-06-17 14:24:12 +0900

    기본 리액트 파일

---
+++ yourtodayis/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Diary from './components/Diary';
+
+function App() {
+  return (
+    <div className="App">
+      <Diary/>
+    </div>
+  );
+}
+
+export default App;
