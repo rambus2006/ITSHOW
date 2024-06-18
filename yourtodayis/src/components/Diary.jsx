8847285f50019b65b260b226b5da9e39ec0e9305
rambus2006 <w2227@e-mirim.hs.kr>
2024-06-18 14:23:41 +0900

    일기장 써지는 거 구현

--- yourtodayis/src/components/Diary.jsx
+++ yourtodayis/src/components/Diary.jsx
@@ -1,16 +1,22 @@
 import './Diary.css'
 import LeftPage from './LeftPage';
 import RightPage from './RightPage';
+import BackgroundComponent from './BackgroundComponent2.jsx';
 
 function Diary() {
   return (
-    // 페이지 div 
+    <>
+    {/* // 페이지 div  */}
     <div className="book-shape-div">
         {/* 페이지 선 div */}
       <LeftPage/>
       <div className="book-shape-line"></div>
       <RightPage/>
     </div>
+    <BackgroundComponent/>
+    </>
+    
+
   );
 }
 
