b0c931a0a4ebf56b7a4d144dc04eafd5f01f18b6
rambus2006 <w2227@e-mirim.hs.kr>
2024-06-18 11:20:16 +0900

    백업

---
+++ Render/threejs-gltf-example/src/Meshes.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+//import {OrbitControls} from '@react-three/drei';
+function CoinMesh() {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.z += 0.1));
+  return (
+    <mesh ref={mesh} scale={3}>
+      <cylinderBufferGeometry color="red" args={[1, 1, 0.3, 50]} />
+      <meshLambertMaterial attach="material" />
+    </mesh>
+  );
+}
+export default function Meshes() {
+  return (
+    <>
+      <Canvas>
+        <CoinMesh />
+      </Canvas>
+    </>
+  );
+}
