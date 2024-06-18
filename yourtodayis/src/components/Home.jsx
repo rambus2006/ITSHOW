59f1cbe29379edcea01a25869ea72468a8ae229a
rambus2006 <w2227@e-mirim.hs.kr>
2024-06-18 16:11:42 +0900

    홈화면까지 완성, 로그인화면부터 작업

--- yourtodayis/src/components/Home.jsx
+++ yourtodayis/src/components/Home.jsx
@@ -1,13 +1,68 @@
-
+import React, { useState } from 'react';
 import BackgroundComponent from './BackgroundComponent2.jsx';
+import BackgroundComponent3 from './BackgroundComponent3.jsx';
+import DiaryCover from './img/DiaryCover.png'; // 이미지 경로 설정
+import './Home.css';
 import Diary from './Diary.jsx';
+import per1img from './img/p1img.png'
+import per2img from './img/p2img.png'
+import lineimg from './img/line.png'
 
 function Home() {
-  return (
-    <div className="App">
-      <Diary/>
-      <BackgroundComponent/>
+  const [showDiary, setShowDiary] = useState(false);
 
+  const handleImageClick = () => {
+    setShowDiary(true);
+  };
+  const handleCloseDiary = () => {
+    setShowDiary(false);
+  };
+  return (
+    <div className="Home">
+      <div className='elements'>
+        <div className='elementstable'>
+          {/* 배경 위 요소들 */}
+          {/* 헤더바 */}
+            <div className='person1'>
+              <div className='person1_img'>
+                <img src={per1img} alt="" />
+              </div>
+              <div className='person1_src'>
+                <h4>김민정</h4>
+                <p>난 겨울이 그렇게 좋더라</p>
+              </div>
+            </div>
+            {/* 라인 */}
+            <div className='lineimg'>
+              <img src={lineimg} alt=''/>
+            </div>
+            <div className='person2'>
+              <div className='person2_img'>
+                  <img src={per2img} alt="" />
+                </div>
+                <div className='person2_src'>
+                  <h4>문태일</h4>
+                  <p>난 여름</p>
+                </div>
+            </div>
+          {/* </div> */}
+          {/* 표지  */}
+          {/* 다이어리 & 앨범이동 */}
+          {!showDiary ? (
+            <div className="img-container" onClick={handleImageClick}>
+              <img src={DiaryCover} alt="Diary Cover" />
+            </div>
+          ) : (
+            <div className="DiaryContainer">
+              <Diary onClose={handleCloseDiary} />
+            </div>
+          )}
+        </div>
+      </div>
+      
+      {/* 배경  */}
+      <BackgroundComponent3 />
+      <BackgroundComponent />
     </div>
   );
 }
