f455cb048216395df8cccf7aa960403e37b8f64b
rambus2006 <w2227@e-mirim.hs.kr>
2024-06-18 11:20:16 +0900

    백업

--- Render/threejs-gltf-example/src/index.js
+++ Render/threejs-gltf-example/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as THREE from 'three';
+import FBXLoader from 'three-fbx-loader';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var loader = new FBXLoader();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var scene = new THREE.Scene();
+
+loader.load('/models/book.fbx', function (object3d) {
+  scene.add(object3d);
+});
+
+// 이후 Three.js의 렌더링 루프 등을 설정할 수 있습니다.
