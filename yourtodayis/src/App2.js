be160eeaa7e529a4bb346adeed227c0e6f9c9bbb
rambus2006 <w2227@e-mirim.hs.kr>
2024-06-19 11:35:26 +0900

    라우터 연결 완료

---
+++ yourtodayis/src/App2.js
@@ -0,0 +1,27 @@
+
+import Home from './components/Home';
+import Index from './components/index';
+import Diary from './components/Diary';
+import Login from './components/Login';
+import Album from './components/Album';
+import Socket from './components/Socket';
+import SocketModel from './components/SocketModel';
+import Logintest from './components/Logintest';
+
+function App() {
+  
+  return (
+    <div className="App">
+      {/* <Home/> */}
+      {/* <Diary/> */}
+      {/* <Login/> */}
+      <Logintest/>
+      {/* <Index/> */}
+      {/* <Album/> */}
+      {/* <Socket/> */}
+      {/* <SocketModel/> */}
+    </div>
+  );
+}
+
+export default App;
